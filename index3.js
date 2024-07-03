cc5f9f70812b92726f589f187d0d97b25ed90c6a
Šárka Flídrová <sarka.flidrova@email.cz>
2024-07-03 14:37:52 +0200

    First commit

---
+++ index3.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const logger = (req, res, next) => {
+  console.log("Request method: " + req.method + " Request url: " + req.url);
+  next();
+};
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
